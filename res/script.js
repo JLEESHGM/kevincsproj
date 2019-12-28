92a220d7fa1702dc5835dfad6c292c5970415810
leekevin20 <leekevin1108@gmail.com>
2019-12-28 19:33:53 +0800

    .

---
+++ res/script.js
@@ -0,0 +1,33 @@
+function buttonClick(name) {
+    if(document.getElementById(name).style.display == "block") {
+      document.getElementById(name).style.display = "none";
+    }
+    else {
+      document.getElementById(name).style.display = "block";
+    }
+}
+
+function func1() {
+  if(document.getElementById('f1q').style.display == "block") {
+    document.getElementById('f1q').style.display = "none";
+  }
+  else {
+    document.getElementById('f1q').style.display = "block";
+  }
+}
+function func2() {
+  if(document.getElementById('f1qq').style.display == "inline") {
+    document.getElementById('f1qq').style.display = "none";
+  }
+  else {
+    document.getElementById('f1qq').style.display = "inline";
+  }
+}
+function func3() {
+  if(document.getElementById('f1qqq').style.display == "inline") {
+    document.getElementById('f1qqq').style.display = "none";
+  }
+  else {
+    document.getElementById('f1qqq').style.display = "inline";
+  }
+}
