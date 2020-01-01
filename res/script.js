a9ed38382dcdc1013ddc8de6d19c8f521a8ba79a
leekevin20 <leekevin1108@gmail.com>
2020-01-01 16:25:16 +0800

    d

--- res/script.js
+++ res/script.js
@@ -6,28 +6,3 @@ function buttonClick(name) {
       document.getElementById(name).style.display = "block";
     }
 }
-
-function func1() {
-  if(document.getElementById('f1q').style.display == "block") {
-    document.getElementById('f1q').style.display = "none";
-  }
-  else {
-    document.getElementById('f1q').style.display = "block";
-  }
-}
-function func2() {
-  if(document.getElementById('f1qq').style.display == "inline") {
-    document.getElementById('f1qq').style.display = "none";
-  }
-  else {
-    document.getElementById('f1qq').style.display = "inline";
-  }
-}
-function func3() {
-  if(document.getElementById('f1qqq').style.display == "inline") {
-    document.getElementById('f1qqq').style.display = "none";
-  }
-  else {
-    document.getElementById('f1qqq').style.display = "inline";
-  }
-}
